b044ea53f39a68d291d15cb82ff1e0408ebafb43
silent-sea <vaughanslater@outlook.com>
2016-01-20 22:41:17 +0000

    Design #5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,4 +4,15 @@
     <div class="row">
         <div class="large-12 columns hero-image">test</div>
     </div>
+
+    <div class="row latest-items">
+        <div class="large-12 columns"><h5>Latest Items:</h5></div>
+    </div>
+
+    <div class="row items">
+        <div class="large-3 columns"><div class="test-item">1</div></div>
+        <div class="large-3 columns"><div class="test-item">1</div></div>
+        <div class="large-3 columns"><div class="test-item">1</div></div>
+        <div class="large-3 columns"><div class="test-item">1</div></div>
+    </div>
 @endsection
