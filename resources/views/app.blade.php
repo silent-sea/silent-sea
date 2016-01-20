eeade00fbd95c686a714687dfffe66f7cf51846d
silent-sea <vaughanslater@outlook.com>
2016-01-20 18:59:51 +0000

    Finished setup

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>App Name - @yield('title')</title>
+    {!! Html::style('css/app.css') !!}
+</head>
+<body>
+
+<div class="container">
+    @yield('content')
+</div>
+
+</body>
+</html>
+
