2347185d2dba66261c51b9e3531635ecb3c8d8ed
silent-sea <vaughanslater@outlook.com>
2016-01-20 19:12:06 +0000

    Fixed issue with Foundation compiling

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,10 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass(
+        'app.scss',
+        'public/css/app.css',
+        { includePaths:
+            ['bower_components/foundation-sites/scss/']
+        });
 });
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,13 @@
 </head>
 <body>
 
+<ul class="menu">
+    <li><a href="#">One</a></li>
+    <li><a href="#">Two</a></li>
+    <li><a href="#">Three</a></li>
+    <li><a href="#">Four</a></li>
+</ul>
+
 <div class="container">
     @yield('content')
 </div>
