f04f0bcdcd5b78ee5718b594a8c3a3ffe63d41d9
silent-sea <vaughanslater@outlook.com>
2016-01-20 22:20:52 +0000

    Design #2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,24 +2,31 @@
 
 <head>
     <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-    <title>App Name - @yield('title')</title>
+    <title>Frog and Veil</title>
     {!! Html::style('css/app.css') !!}
 </head>
 
 <body>
-    <div class="container" style="margin-top: 100px;">
+    <div class="container" style="margin-top: 150px;">
         <div class="row topnav">
-            <div class="small-2 large-2 columns tc">HOME</div>
-            <div class="small-2 large-2 columns tc">COLLECTIONS</div>
-            <div class="small-2 large-2 columns tc">DETAILS & PRICES</div>
-            <div class="small-2 large-2 columns tc">ABOUT</div>
-            <div class="small-2 large-2 columns tc">BLOG</div>
-            <div class="small-2 large-2 columns tc">CONTACT</div>
+            <div class="small-2 large-2 columns tc"><a href="#">HOME</a></div>
+            <div class="small-2 large-2 columns tc"><a href="#">COLLECTIONS</a></div>
+            <div class="small-2 large-2 columns tc"><a href="#">DETAILS & PRICES</a></div>
+            <div class="small-2 large-2 columns tc"><a href="#">ABOUT</a></div>
+            <div class="small-2 large-2 columns tc"><a href="#">BLOG</a></div>
+            <div class="small-2 large-2 columns tc"><a href="#">CONTACT</a></div>
         </div>
     </div>
 
     <div class="container">
         @yield('content')
+    </div>
+
+    <div class="container">
+        <div class="row footer">
+            <div class="left">Copyright &copy; Frog and Veil 2016</div>
+            <div class="right">Contact</div>
+        </div>
     </div>
 </body>
 
