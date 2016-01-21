e3bc1b2df274555e5c08e9f8c69ff328b2e3ccec
silent-sea <vaughanslater@outlook.com>
2016-01-21 23:15:12 +0000

    Re-design #7

--- resources/views/layouts/welcomehero.blade.php
+++ resources/views/layouts/welcomehero.blade.php
@@ -1,7 +1,7 @@
 <div class="row margin-bottom: 50px;">
     <div id="owl" class="owl-carousel owl-theme">
         <div class="item"><img src="http://www.95wowgold.com/images/exceptional-shine-wedding-invitations-5-wedding-invitation-1200-x-900.jpg" alt="The Last of us"></div>
-        <div class="item"><img src="http://www.95wowgold.com/images/exceptional-shine-wedding-invitations-5-wedding-invitation-1200-x-900.jpg" alt="GTA V"></div>
-        <div class="item"><img src="http://www.95wowgold.com/images/exceptional-shine-wedding-invitations-5-wedding-invitation-1200-x-900.jpg" alt="Mirror Edge"></div>
+        <div class="item"><img src="https://www.thelowryhotel.com/assets/lowry_hotel/content/images/1200x559_wedding%20fayre%20-%20table%20setting%201-large-carousel.jpg" alt="GTA V"></div>
+        <div class="item"><img src="http://cdni.condenast.co.uk/1200x1200/k_n/lookbook_brd_9dec15_catherinegratwicke_b_cp.jpg" alt="Mirror Edge"></div>
     </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,23 +2,27 @@
 
 @section('content')
 
-
     @include('layouts.welcomehero')
 
+    <div class="row section">
+        <h2 class="section__title">SOME TITLE</h2>
+        <p class="section__intro">
+            Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas faucibus mollis interdum. Curabitur blandit tempus porttitor.
+        </p>
+    </div>
+
     <div class="row">
-        <div class="box-grid">
-            <div class="box">
-                <h3>Creative Idea</h3>
-                <p>Duis neque leo cursus ac libero ac imperdiet bibendum nisl mauris neque diam vulputate nem.</p>
-            </div>
-            <div class="box">
-                <h3>Creative Idea</h3>
-                <p>Duis neque leo cursus ac libero ac imperdiet bibendum nisl mauris neque diam vulputate nem.</p>
-            </div>
-            <div class="box">
-                <h3>Creative Idea</h3>
-                <p>Duis neque leo cursus ac libero ac imperdiet bibendum nisl mauris neque diam vulputate nem.</p>
-            </div>
+        <div class="box__grid">
+            <article class="box"><a href="#" class="box__content"><i class="fa fa-lightbulb-o fa-3x"></i>
+                    <h3 class="box__title">Creative Ideas</h3>
+                    <p>Duis neque leo cursus ac libero ac imperdiet bibendum nisl mauris neque diam vulputate nem.</p><span class="box__more">Read more</span></a></article>
+            <article class="box"><a href="#" class="box__content"><i class="fa fa-lightbulb-o fa-3x"></i>
+                    <h3 class="box__title">Creative Ideas</h3>
+                    <p>Duis neque leo cursus ac libero ac imperdiet bibendum nisl mauris neque diam vulputate nem.</p><span class="box__more">Read more</span></a>
+            </article>
+            <article class="box"><a href="#" class="box__content"><i class="fa fa-lightbulb-o fa-3x"></i>
+                    <h3 class="box__title">Creative Ideas</h3>
+                    <p>Duis neque leo cursus ac libero ac imperdiet bibendum nisl mauris neque diam vulputate nem.</p><span class="box__more">Read more</span></a></article>
         </div>
     </div>
 @endsection
